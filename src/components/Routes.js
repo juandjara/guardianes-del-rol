f6dd09e8020e883286f94477889fd17c928c2e12
Juan D. Jara <juanorigami@gmail.com>
2019-04-23 01:02:30 +0200

    remove commented routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,10 +5,7 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import Login from './auth/Login';
 import PostList from './post/PostList';
 import PostEdit from './post/PostEdit';
-import CategoryList from './category/CategoryList';
-import CategoryEdit from './category/CategoryEdit';
 import NotFound from './NotFound';
-import ImageGallery from './image-gallery/ImageGallery';
 import Profile from './auth/Profile';
 import ProtectedRoute from './auth/ProtectedRoute';
 import LoginConfirm from './auth/LoginConfirm';
@@ -28,9 +25,6 @@ const Routes = props => props.loadingAuth ? <Loading /> : (
     <ProtectedRoute user={props.user} exact path="/post" component={PostList} />
     <ProtectedRoute exact user={props.user} path="/post/:id" component={PostDisplay} />
     <ProtectedRoute exact user={props.user} path="/post/:id/edit" component={PostEdit} />
-    {/* <ProtectedRoute user={props.user} exact path="/category" component={CategoryList} /> */}
-    {/* <ProtectedRoute user={props.user} path="/category/:id" component={CategoryEdit} /> */}
-    {/* <ProtectedRoute user={props.user} path="/gallery" component={ImageGallery} /> */}
     <ProtectedRoute user={props.user} path="/profile" component={Profile} /> 
     <Route component={NotFound} />
   </Switch>
