885d14810acfe2d0a322bb7df31ce6b6672e5cea
Juan D. Jara <juanorigami@gmail.com>
2019-05-08 14:10:20 +0200

    footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const PrivacyLinkContainer = styled.footer`
   position: fixed;
   bottom: 0;
   right: 0;
-  background-color: rgba(255, 255, 255, 0.85);
+  background-color: rgba(255, 255, 255, 0.9);
   border-top-left-radius: 4px;
   padding: 0 12px;
   line-height: 24px;
@@ -18,13 +18,14 @@ const CookieWindow = styled.div`
   position: fixed;
   bottom: 0;
   left: 0;
-  background-color: rgba(255, 255, 255, 0.85);
+  background-color: rgba(255, 255, 255, 0.9);
   border-top-right-radius: 4px;
   padding: 6px 12px;
   font-size: 14px;
   display: flex;
   align-items: center;
   max-width: 600px;
+  z-index: 2;
   a {
     flex-shrink: 0;
   }
